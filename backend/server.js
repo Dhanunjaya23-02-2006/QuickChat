1cf0fd1766934b3cddf3ad4acc2dfca916bc2c7d
dhanunjaya12366 <168557877+dhanunjaya12366@users.noreply.github.com>
2026-09-16 19:48:55 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,11 @@ app.use('/api/messages', messageRoute)
 
 await connectDB()
 
-const PORT = process.env.PORT || 5000
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000
+    server.listen(PORT, () => console.log("Server is running on PORT: "+PORT))
+}
 
-server.listen(PORT, () => console.log("Server is running on PORT: "+PORT))
+export default server;
+
 
